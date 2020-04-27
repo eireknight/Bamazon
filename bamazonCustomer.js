b8e4a8d32141a1856b29686b65e37d7860132b53
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-04-27 14:48:23 -0400

    Mostly working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,51 +1,84 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-// connect to the mysql database
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8889,
+  port: 3306,
   user: "root",
-  password: "root",
+  password: "PNOFreedom14!",
   database: "bamazon_db"
 });
 
-//tests connection
 connection.connect(function(err){
-  if(err){
-    console.log(err);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+  if (err) throw err;
+  console.log("Connected: ID " + connection.threadId);
   displayInventory();
 });
 
-// inquirer prompts
-var productPurchasePrompt = {
+var Purchase = {
   type:'input',
-  message:"Type in the ID of the product you would like to purchase:",
-  name:'product_purchase'
+  message:"What would you like to purchase?  Enter the ID",
+  name:'Purchase'
 };
-var productQuantityPrompt = {
+var Quantity = {
   type:'input',
-  message:"How many units of the product would you like to purchase?",
-  name:'product_quantity'
+  message:"How many would you like to purchase?",
+  name:'Quantity'
 };
-var restartPrompt = {
+var Restart = {
   type: "list",
-  message: "Would you like to shop again?",
+  message: "Would you like to continue shopping?",
   choices: ["Yes", "No"],
-  name: "restart_prompt"
+  name: "Restart"
 };
 
-// displays all inventory when first running application
 var displayInventory = function(){
-  connection.query("SELECT * FROM products", function(err,res){
-    console.log("DISPLAYING ALL INVENTORY:" + "\n" + "----------------------------");
+  connection.query("SELECT * FROM inventory", function(err,res){
+    console.log("Displaying Inventory:" + "\n" + "----------------------------");
     for (var i = 0; i < res.length; i++) {
       console.log("Item ID: " + res[i].item_id + "\n" + "Product Name: " + res[i].product_name + "\n" + "Price: " + res[i].price + "\n" + "Available Quantity: " + res[i].stock_quantity + "\n----------------------------");
     }
-    // then prompts users with which products they would like to buy
     promptCustomer(res);
+  })
+}
+
+var promptCustomer = function(res){
+  inquirer.prompt([Purchase]).then(function(inquirerResponse){
+    var chosenProductID = parseInt(inquirerResponse.Purchase);
+    for (var i=0;i<res.length;i++){
+      if(res[i].item_id === chosenProductID){
+        var id = i;
+        inquirer.prompt([Quantity]).then(function(inquirerResponse){
+          var chosenQuantity = parseInt(inquirerResponse.Quantity);
+
+          if ((res[id].stock_quantity - chosenQuantity) >= 0) {
+            var newQuantity = res[id].stock_quantity - chosenQuantity;
+            var totalCost = res[id].price * chosenQuantity;
+            var sql = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+            var values = ['product_name', 'stock_quantity', newQuantity, 'item_id', chosenProductID];
+            connection.query(sql, values, function(err, res){
+              if(err) {
+                throw err,
+                connection.end();
+              }
+              console.log("Product(s) bought!" + "\n" + "Total Cost of Transaction: $" + totalCost);
+              inquirer.prompt([Restart]).then(function(inquirerResponse){
+                if (inquirerResponse.Restart === "Yes") {
+                  displayInventory();
+                } else {
+                  console.log("Thank You!");
+                  connection.end();
+                }
+              })
+            })
+          }
+
+          else {
+            console.log("Insufficient Quantity! We only carry in stock the quantity shown.");
+            promptCustomer(res);
+          }
+        })
+      }
+    }
   })
 }
